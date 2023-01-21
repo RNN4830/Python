ef062e43d2a52f006455038181ba3730420b9052
Rachel Nelson <april30angel@msn.com>
2023-01-21 11:56:03 -0700

    odd or even python

---
+++ odd_or_even.py
@@ -0,0 +1,10 @@
+
+# ask user for a number
+# determine if the number is even or odd
+
+number = int(input("Please pick a number to determine if it is even or odd: "))
+
+if number % 2 == 0:
+    print ("The number is even!")
+else:
+    print ("The number is odd!")
