950d8ff1c09b72871083e8d948ce5931bbb5222c
Rachel Nelson <april30angel@msn.com>
2023-01-22 05:54:58 -0700

    reverse string order

---
+++ reverse_sentence.py
@@ -0,0 +1,13 @@
+
+#take input from user
+#split the input from user at the spaces and add to list
+#reverse list
+#print words in reverse
+
+
+userInput = input("Please enter a sentence to see it backwards: ")
+print (userInput)
+splitList = userInput.split(" ")
+reverseList = splitList[::-1]
+# print(reverseList)
+print(*reverseList)
